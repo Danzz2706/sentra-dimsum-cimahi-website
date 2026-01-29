0d4d3a9b08898bc9b331679152e42dfc1481be27
Danzz2706 <munadizaidan@gmail.com>
2026-01-29 20:46:24 +0700

    update icon

--- src/app/manifest.js
+++ src/app/manifest.js
@@ -9,14 +9,14 @@ export default function manifest() {
         theme_color: '#F97316',
         icons: [
             {
-                src: '/logo.png',
+                src: '/logo.jpeg',
                 sizes: '192x192',
-                type: 'image/png',
+                type: 'image/jpeg',
             },
             {
-                src: '/logo.png',
+                src: '/logo.jpeg',
                 sizes: '512x512',
-                type: 'image/png',
+                type: 'image/jpeg',
             },
         ],
     }
